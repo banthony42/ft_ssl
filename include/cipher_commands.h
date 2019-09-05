61b882cc39e7731a8e19a15bf5b0b9976773bf2c
banthony42 <banthony@student.42.fr>
2019-09-05 21:20:48 +0200

    [BASE64] Update files according to the 42 norm. * Delete useless folder and file. * Add folder for unit test and error file.

--- include/cipher_commands.h
+++ include/cipher_commands.h
@@ -6,14 +6,14 @@
 /*   By: abara <banthony@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/19 12:40:36 by abara             #+#    #+#             */
-/*   Updated: 2019/07/26 14:53:19 by abara            ###   ########.fr       */
+/*   Updated: 2019/09/05 21:07:46 by banthony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CIPHER_COMMANDS_H
-#define CIPHER_COMMANDS_H
+# define CIPHER_COMMANDS_H
 
-#include "ft_ssl.h"
+# include "ft_ssl.h"
 
 /*
 **	************************ BASE64 ************************
@@ -23,39 +23,48 @@
 **	BASE64 options & MASK
 */
 
-#define BASE64_OPTS "-d;-e"
-#define B64_DECODE_MASK 1
-#define B64_ENCODE_MASK 1 << 1
+# define BASE64_OPTS "-d;-e"
+# define B64_DECODE_MASK 1
+# define B64_ENCODE_MASK 1 << 1
 
 /*
 **	Option avec entree utilisateur
 */
 
-#define BASE64_INPUT_FILE_KEY "-i"
-#define BASE64_OUTPUT_FILE_KEY "-o"
+# define BASE64_INPUT_FILE_KEY "-i"
+# define BASE64_OUTPUT_FILE_KEY "-o"
 
 /*
 **	Definit si un algorithme de chiffrage doit decoder ou encoder.
 */
-typedef enum e_cipher_mode
+typedef enum		e_cipher_mode
 {
 	ENCODE,
 	DECODE,
-} t_cipher_mode;
+}					t_cipher_mode;
 
 /*
 **	cipher_mode: Mode de fonctionnement (Encodage ou Decodage)
 **	inp_fd:		 Source pour le chiffrage, STDIN par defaut
 **	out_fd:		 Sortie pour le chiffrage, STDOUT par defaut
 */
-typedef struct s_base64
+typedef struct		s_base64
 {
-	t_cipher_mode cipher_mode;
-	t_bool b64_url;
-	int in_fd;
-	int out_fd;
-} t_base64;
+	t_cipher_mode	cipher_mode;
+	t_bool			b64_url;
+	int				in;
+	int				out;
+}					t_base64;
 
-void base64_cipher(t_base64 b64, char *entry);
+typedef struct		s_decode_block
+{
+	char			char_array[8];
+	int				i_0;
+	int				i_1;
+	int				i_2;
+	int				i_3;
+}					t_decode_block;
+
+void				base64_cipher(t_base64 b64, char *entry);
 
 #endif
--- src/base64_cipher.c
+++ src/base64_cipher.c
@@ -6,137 +6,168 @@
 /*   By: abara <banthony@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 14:21:32 by abara             #+#    #+#             */
-/*   Updated: 2019/07/26 14:53:09 by abara            ###   ########.fr       */
+/*   Updated: 2019/09/05 21:16:20 by banthony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cipher_commands.h"
 
-static const char g_base64_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-static const char g_base64_url_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+static const char g_base64_table[65] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-	// If file input and entry are both present,
-	// determine which data will be ciphered.
+static const char g_base64_url_table[65] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
-static void encode_b64(char *entry, size_t entry_len, unsigned char padding, t_base64 b64)
+// TODO check the following comment
+
+/*
+**	If file input and entry are both present,
+**	determine which data will be ciphered.
+**
+**	Have to handle invalid character in decode mode, example: '.'
+**
+**	Maybe we need to store out[i] and shift bit using the storage variable.
+**	to fix the issue with the file_error_1.
+*/
+
+/*
+**	If padding == 4:
+**	At the end of the loop there is still 8bits unused, so
+**	we have added 4 bits set to zero, then we can read the 12 last bits.
+**
+**	If paddding == 2:
+**	At the end of the loop there is still 16bits unused, so
+**	we have added 2 bits set to zero, then we can read the 18 last bits.
+*/
+
+static void	encode_b64_end(char pad, char *out, t_base64 b64, char table[65])
 {
-	char			*end;
-	char			*output;
-	unsigned char	newline_counter;
-	char table[65] = {0};
-
-	(b64.b64_url == true) ? ft_strncpy(table, g_base64_url_table, 65) : ft_strncpy(table, g_base64_table, 65);
-	newline_counter = 0;
-	output = entry;
-	end = entry + entry_len;
-	while ((end - output) >= 3)
+	if (pad == 4)
 	{
-		if (newline_counter >= 63) {
-			ft_putchar_fd('\n', b64.out_fd);
-			newline_counter = 0;
-		}
-		ft_putchar_fd(table[output[0] >> 2], b64.out_fd);
-		ft_putchar_fd(table[((output[0] & 0x03) << 4) | output[1] >> 4], b64.out_fd);
-		ft_putchar_fd(table[((output[1] & 0xf ) << 2) | output[2] >> 6], b64.out_fd);
-		ft_putchar_fd(table[((output[2] & 0x3f))], b64.out_fd);
-		newline_counter += 4;
-		output += 3;
+		ft_putchar_fd(table[out[0] >> 2], b64.out);
+		ft_putchar_fd(table[((out[0] & 0x03) << 4) | out[1] >> 4], b64.out);
+		ft_putstr_fd("==", b64.out);
 	}
-	// Read 12 bits and add "=="
-	if (padding == 4)
+	if (pad == 2)
 	{
-		ft_putchar_fd(table[output[0] >> 2], b64.out_fd);
-		ft_putchar_fd(table[((output[0] & 0x03) << 4) | output[1] >> 4], b64.out_fd);
-		ft_putstr_fd("==", b64.out_fd);
+		ft_putchar_fd(table[out[0] >> 2], b64.out);
+		ft_putchar_fd(table[((out[0] & 0x03) << 4) | out[1] >> 4], b64.out);
+		ft_putchar_fd(table[((out[1] & 0xf) << 2) | out[2] >> 6], b64.out);
+		ft_putchar_fd('=', b64.out);
 	}
-	// Read 18 bits and add "="
-	if (padding == 2)
-	{
-		ft_putchar_fd(table[output[0] >> 2], b64.out_fd);
-		ft_putchar_fd(table[((output[0] & 0x03) << 4) | output[1] >> 4], b64.out_fd);
-		ft_putchar_fd(table[((output[1] & 0xf ) << 2) | output[2] >> 6], b64.out_fd);
-		ft_putchar_fd('=', b64.out_fd);
-	}
-	ft_putchar_fd('\n', b64.out_fd);
 }
 
-	//	Block of 3 octet, split into 4 field of 6 bits :
-	//	0b011000|11		'c'
-	//	0b0110|1111		'o'
-	//	0b01|110101		'u'
-	//
-	//	Give us :
-	//
-	//	00|011000	24	'Y'
-	//	00|110110	54	'2'
-	//	00|111101	61	'9'
-	//	00|110101	53	'1'
+/*
+**	Parse entry by block of 3 char because : (3 * 8 bits = 24 bits)
+**	Read all the 24 bits by step of 6 bits.
+**	Use the value of this 6 bits for each step, as index
+**	in the base64_table / base64_url_table.
+**	This give us the cipher for the current block of 3 char.
+**	At the end read the last 12 bits or 18bits and put some '='.
+**
+**	Example with the entry "cou"
+**	Block of 3 octet, split into 4 field of 6 bits :
+**
+**	0b011000|11		'c'
+**	0b0110|1111		'o'
+**	0b01|110101		'u'
+**
+**	A block of 6 bits give us a number, use it in the base64 table:
+**
+**	00|011000	24	'Y'
+**	00|110110	54	'2'
+**	00|111101	61	'9'
+**	00|110101	53	'1'
+*/
 
-// 00|000010	'C'
-// 00|100000	'g'
-// 000000000	'='
-// 000000000	'='
-
-// 00001010 00000000 00000000 00000000
-
-static void decode_b64(char *entry, size_t entry_len, t_base64 b64)
+static void	encode_b64(char *entry, char padding, t_base64 b64, char table[65])
 {
-	// Have to parse base 64 string, foreach block of 4 char, encode 24 bits.
-	// ft_putstr("==[");
-	// ft_putstr(entry);
-	// ft_putendl("]==");
-	char table[65] = {0};
+	char			*end;
+	char			*out;
 
-	(b64.b64_url == true) ? ft_strncpy(table, g_base64_url_table, 65) : ft_strncpy(table, g_base64_table, 65);
-
-	// Build of the decode table, to transform char into index of g_base64_table
-	int base64_decode_table[255];
-	ft_memset(base64_decode_table,'#', sizeof(base64_decode_table));
-	int j = -1;
-	while (++j < (int)sizeof(table))
-		base64_decode_table[(int)table[j]] = j;
-	base64_decode_table['='] = 0;
-
-	size_t i = 0;
-	char block[5] = {0};
-	while (i < entry_len)
+	out = entry;
+	end = entry + ft_strlen(entry);
+	while ((end - out) >= 3)
 	{
-		ft_strncat(block, &entry[i], 1);
-		if (ft_strlen(block) >= 4){
-			int ib64_0 = base64_decode_table[(int)block[0]];
-			int ib64_1 = base64_decode_table[(int)block[1]];
-			int ib64_2 = base64_decode_table[(int)block[2]];
-			int ib64_3 = base64_decode_table[(int)block[3]];
+		ft_putchar_fd(table[out[0] >> 2], b64.out);
+		ft_putchar_fd(table[((out[0] & 0x03) << 4) | out[1] >> 4], b64.out);
+		ft_putchar_fd(table[((out[1] & 0xf) << 2) | out[2] >> 6], b64.out);
+		ft_putchar_fd(table[((out[2] & 0x3f))], b64.out);
+		out += 3;
+	}
+	encode_b64_end(padding, out, b64, table);
+	ft_putchar_fd('\n', b64.out);
+}
 
-			ft_putchar_fd((char)((ib64_0 << 2) | (ib64_1 ) >> 4), b64.out_fd);
-			ft_putchar_fd((char)((ib64_1 << 4) | (ib64_2 ) >> 2), b64.out_fd);
-			ft_putchar_fd((char)((ib64_2 << 6) | (ib64_3 ) ), b64.out_fd);
-			ft_memset(block, 0, 4);
+/*
+**	Parse entry by block of 4 char because we are in decode mode:
+**	In base64, a ciphered char is coded on 6 bits.
+**	If we parse the entry by block of 4 ciphered char we have: 4 * 6 = 24 bits.
+**	Then we have to concat this bits and split it into 3 bytes: 3 * 8 = 24 bits.
+**	Finally we have our 3 deciphered char.
+**
+**	The deciphering works with a decode table.
+**	This decode table was built with the encode table using this tricks:
+**	decode_table[encode_table[i]] = i;
+**
+**	Example with i = 0 : where  encode_table[0] = 'A'
+**	decode_table[ 'A' ] = 0;
+**
+**	The tricks is to use ascii value of 'A' as index to store the value of 'A'.
+*/
+
+static void	decode_b64(char *entry, t_base64 b64, int b64_decode[255])
+{
+	size_t			i;
+	size_t			len;
+	t_decode_block	block;
+
+	i = 0;
+	len = ft_strlen(entry);
+	ft_memset(&block, 0, sizeof(block));
+	while (i < len)
+	{
+		ft_strncat(block.char_array, &entry[i], 1);
+		if (ft_strlen(block.char_array) >= 4)
+		{
+			block.i_0 = b64_decode[(int)block.char_array[0]];
+			block.i_1 = b64_decode[(int)block.char_array[1]];
+			block.i_2 = b64_decode[(int)block.char_array[2]];
+			block.i_3 = b64_decode[(int)block.char_array[3]];
+			ft_putchar_fd((char)((block.i_0 << 2) | block.i_1 >> 4), b64.out);
+			ft_putchar_fd((char)((block.i_1 << 4) | block.i_2 >> 2), b64.out);
+			ft_putchar_fd((char)((block.i_2 << 6) | block.i_3), b64.out);
+			ft_memset(block.char_array, 0, 4);
 		}
 		i++;
 	}
-	// Last block with padding treatment
-	// ft_putendl(block);
 }
 
-void	base64_cipher(t_base64 b64, char *entry)
+void		base64_cipher(t_base64 b64, char *entry)
 {
-	size_t			entry_len;
-	unsigned char	padding;
+	char			padding;
+	char			table[65];
+	int				b64_decode[255];
+	int				j;
 
 	if (!entry)
 		return ;
-	entry_len = ft_strlen(entry);
+	(b64.b64_url == true) ? ft_strncpy(table, g_base64_url_table, 65)
+		: ft_strncpy(table, g_base64_table, 65);
 	if (b64.cipher_mode == ENCODE)
 	{
-		if ((padding = (entry_len * 8) % 24) == 8)
+		if ((padding = (ft_strlen(entry) * 8) % 24) == 8)
 			padding = 4;
-		else if (padding == 16)
-			padding = 2;
 		else
-			padding = 0;
-		encode_b64(entry, entry_len, padding, b64);
+			padding = (padding == 16) ? (2) : (0);
+		encode_b64(entry, padding, b64, table);
 	}
 	else if (b64.cipher_mode == DECODE)
-		decode_b64(entry, entry_len, b64);
+	{
+		j = -1;
+		ft_memset(b64_decode, 0, sizeof(b64_decode));
+		while (++j < (int)sizeof(table))
+			b64_decode[(int)table[j]] = j;
+		decode_b64(entry, b64, b64_decode);
+	}
 }
